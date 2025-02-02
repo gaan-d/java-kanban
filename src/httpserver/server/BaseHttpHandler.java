acea64c4f3ccb76901e22b384b7fb9da17c8f006
Danil <ynx-gaan-d@yandex.ru>
2025-02-03 03:32:51 +0500

    Optimize imports

--- src/httpserver/server/BaseHttpHandler.java
+++ src/httpserver/server/BaseHttpHandler.java
@@ -4,7 +4,6 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 public abstract class BaseHttpHandler implements HttpHandler {
